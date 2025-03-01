3dc4fc2ec89388bdd25fb97ca295c45351d714d4
AlanMascorro <106097438+AlanMascorro@users.noreply.github.com>
2025-02-28 18:28:28 -0600

    Update doc_opinion.py

--- doc_opinion.py
+++ doc_opinion.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import sys
 import json
 
-genai.configure(api_key="AIzaSyCC8oA2UchmPrJzDSyvSGVgitDLjC9wP9U")
+genai.configure(api_key="Your key")
 model = genai.GenerativeModel("gemini-1.5-flash")
 #with open("ill.json", "r", encoding="utf-8") as f:
  #       sick_list = json.load(f)
