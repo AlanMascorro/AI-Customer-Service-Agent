d63566da8322cfd0db64db69389365b7bfc8752c
Micah Moon <108433029+GibbusMoon@users.noreply.github.com>
2025-04-22 14:27:53 -0500

    Update ChatBox.vue
    Deleted port

--- frontend/src/views/ChatBox.vue
+++ frontend/src/views/ChatBox.vue
@@ -73,7 +73,7 @@
       
         this.messages.push({ text: this.userInput, sender: "user" });
 
-        const url = "https://" + process.env.VUE_APP_HOST_IP + ":" + process.env.VUE_APP_HOST_PORT + "/api/query";
+        const url = "https://" + process.env.VUE_APP_HOST_IP /* + ":" + process.env.VUE_APP_HOST_PORT */ + "/api/query";
                 
 
         const response = await axios.get(url, { 
