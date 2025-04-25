6ad80da05a134600af0e531964caaca046ce25f1
Zaaim Rahman <zaaimrahman.comp@gmail.com>
2025-04-25 11:21:00 -0500

    update the origin for cors

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const app = express();
 //--Middleware Activation--//
 app.use(express.json()); 
 app.use(cors({
-  origin: 'https://ai-customer-service-agent-1.onrender.com',
+  origin: 'https://ai-customer-service-agent.onrender.com',
   credentials: true
 }));
 //app.use(express.static(path.join(__dirname, "frontend", "dist")));
