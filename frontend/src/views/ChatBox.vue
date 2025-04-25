58adc846334723c1427d4ffc193944347dda8d81
Zaaim Rahman <zaaimrahman.comp@gmail.com>
2025-04-25 10:59:54 -0500

    try changing url

--- frontend/src/views/ChatBox.vue
+++ frontend/src/views/ChatBox.vue
@@ -68,7 +68,7 @@
  // var oldy;
   var tween;
 
-  let urlClient = "https://" + process.env.VUE_APP_HOST_IP + ":" + process.env.VUE_APP_HOST_PORT + "/api/client";
+  let urlClient = process.env.VUE_APP_HOST_IP + ":" + process.env.VUE_APP_HOST_PORT + "/api/client";
   //urlClient = "api/client";
   const serverClient = await axios.get(urlClient);
   console.log(serverClient.data.clientID);
